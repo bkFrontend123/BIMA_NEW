c3cbac35516ac4003b89ac0f2aa71565fc3ffa47
Frontend Bimakavach <frontend@bimakavach.com>
2023-09-27 16:29:48 +0530

    Update

--- component/GHI/ProductContent/ProductContentRight/index.js
+++ component/GHI/ProductContent/ProductContentRight/index.js
@@ -9,16 +9,48 @@ import style from './style.module.css'
 
 export default function ProductContentRight(props) {
     const { onClick } = props;
+
+    const coverageData = ([
+        {
+          id: "1",
+          icon: "/productIcons/liability/dAndO_icon.svg",
+          title: "Answer a few Simple Questions",
+          description: "Directors and officers (D&O) liability insurance is insurance coverage intended to protect individuals from personal losses.",
+          link: "/directors-and-officers-insurance",
+          flowLink: "/directors-and-officers-flow"
+        },
+        {
+          id: "2",
+          icon: "/productIcons/liability/eAndO_icon.svg",
+          title: "Errors & Omissions Insurance",
+          description: "Errors and omissions (E&O) insurance is a type of professional liability insurance that protects companies, their workers.",
+          link: "/errors-and-omissions-insurance",
+          flowLink: "/errors-and-omissions-flow"
+        },
+        {
+          id: "3",
+          icon: "/productIcons/liability/cyber_icon.svg",
+          title: "Cyber Insurance",
+          description: "A cyber insurance coverage policy can provide companies with protection from losses resulting from a data breach.",
+          link: "/cyber-insurance",
+          flowLink: "/cyber-flow"
+        },
+    ])
+
     return (
         <>
             <div className={`${style.prdctCntntRghtCol}`}>
-                <ProductInfoCard
-                    title="Answer a few Simple Questions"
-                    icon={icon1}
-                    width={165}
-                    height={196}
-                    onClick={onClick}
-                />
+                {coverageData.map((data, index) =>
+                    <ProductInfoCard
+                        title="Answer a few Simple Questions"
+                        icon={icon1}
+                        width={165}
+                        height={196}
+                        onClick={onClick}
+                        key={index}
+                        {...data}
+                    />
+                )}
                 <ProductInfoCard
                     title="Get curated Quotes
                     in Minutes"
